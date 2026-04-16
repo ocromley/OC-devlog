81e7ce88e6a59c4c5bc2d549a2038a78c32abd09
ocromley <ocromley@icstars.org>
2026-04-15 20:14:01 -0500

    Clean up node_modules from git history

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/OC-devlog/', // Add this line! Use your exact repo name
   plugins: [react()],
   server: {
     proxy: {
-      // This tells Vite: "If a request starts with /api, 
-      // secretly send it to our Express server on port 4000"
       '/api': 'http://localhost:4000',
     },
   },
